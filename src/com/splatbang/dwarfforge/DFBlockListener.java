6821c0b34575ddc81904c3d40ddde6e5cadc960f
Matthew Moss <matt@moss.name>
2011-06-17 09:14:29 -0500

    simpler first version; punch furnace to fire it up

--- src/com/splatbang/dwarfforge/DFBlockListener.java
+++ src/com/splatbang/dwarfforge/DFBlockListener.java
@@ -1,54 +1,40 @@
 package com.splatbang.dwarfforge;
 
-import java.lang.Runnable;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Player;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Furnace;
 import org.bukkit.event.block.BlockListener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 
-public class DFBlockListener extends BlockListener implements Runnable {
+public class DFBlockListener extends BlockListener {
 
     private DwarfForge plugin;
+
+    private static final short SECS = 20;
+    private static final short MINS = 60 * SECS;
+    private static final short DURATION = 25 * MINS;    // < 32767: max short
 
     public DFBlockListener(DwarfForge plugin) {
         this.plugin = plugin;
     }
 
-    public void onBlockBreak(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        Block block = event.getBlock();
-    }
-
     public void onBlockDamage(BlockDamageEvent event) {
-        Player player = event.getPlayer();
         Block block = event.getBlock();
-    }
+        BlockState blockState = block.getState();
 
-    public static boolean isLavaBelow(Block block) {
-        Block below = block.getRelative(BlockFace.DOWN);
-        return (below.getType() == Material.LAVA) || (below.getType() == Material.STATIONARY_LAVA);
-    }
-
-    public void onBlockPlace(BlockPlaceEvent event) {
-        Player player = event.getPlayer();
-        Block block = event.getBlock();
-
-        if (!event.isCancelled() && block.getType() == Material.FURNACE) {
+        if (blockState instanceof Furnace) {
             if (isLavaBelow(block)) {
-                plugin.logInfo(player.getName() + " placed furnace above lava.");
-            }
-            else {
-                plugin.logInfo(player.getName() + " placed furnace above something other than lava.");
+                Furnace f = (Furnace) blockState;
+                f.setBurnTime(DURATION);
             }
         }
     }
 
-    public void run() {
-        //plugin.logInfo("DFBlockListener furnace task fired.");
+    private static boolean isLavaBelow(Block block) {
+        Block below = block.getRelative(BlockFace.DOWN);
+        return (below.getType() == Material.LAVA) || (below.getType() == Material.STATIONARY_LAVA);
     }
 
 }
--- src/com/splatbang/dwarfforge/DwarfForge.java
+++ src/com/splatbang/dwarfforge/DwarfForge.java
@@ -1,8 +1,6 @@
 package com.splatbang.dwarfforge;
 
 import java.lang.String;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 import org.bukkit.Server;
 import org.bukkit.event.Event;
@@ -15,23 +13,10 @@ public class DwarfForge extends JavaPlugin {
 
     private DFBlockListener blockListener = new DFBlockListener(this);
 
-    private final int NO_TASK = -1;
-    private final long SECONDS = 20L;
-    private int furnaceTask = NO_TASK;
-
     @Override
     public void onEnable() {
-        setupDatabase();
-
         PluginManager pm = getServer().getPluginManager();
-        pm.registerEvent(Event.Type.BLOCK_BREAK,  blockListener, Event.Priority.Monitor, this);
         pm.registerEvent(Event.Type.BLOCK_DAMAGE, blockListener, Event.Priority.Monitor, this);
-        pm.registerEvent(Event.Type.BLOCK_PLACE,  blockListener, Event.Priority.Monitor, this);
-
-        furnaceTask = getServer().getScheduler().scheduleSyncRepeatingTask(this, blockListener, 30*SECONDS, 30*SECONDS);
-        if (furnaceTask == NO_TASK) {
-            logSevere("Could not schedule furnace task.");
-        }
 
         PluginDescriptionFile pdf = this.getDescription();
         logInfo("Version " + pdf.getVersion() + " enabled.");
@@ -39,29 +24,11 @@ public class DwarfForge extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        if (furnaceTask != NO_TASK) {
-            getServer().getScheduler().cancelTask(furnaceTask);
-            furnaceTask = NO_TASK;
-        }
         logInfo("Disabled.");
-    }
-
-    private void setupDatabase() {
-    }
-
-    @Override
-    public List<Class<?>> getDatabaseClasses() {
-        List<Class<?>> list = new ArrayList<Class<?>>();
-        // ...
-        return list;
     }
 
     public void logInfo(String msg) {
         log.info("[DwarfForge] " + msg);
-    }
-
-    public void logSevere(String msg) {
-        log.severe("[DwarfForge] " + msg);
     }
 
 }
